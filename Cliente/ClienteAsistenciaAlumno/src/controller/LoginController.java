d25d12e8cd5aa9c47f9ffd27e88d2c46c3cb3606
CARLOS ANTONIO MATICH CUEZZO <98784204+Nippur7@users.noreply.github.com>
2025-06-02 23:24:16 -0300

    Client Side
    The client is a part that allows track the student activities

---
+++ Cliente/ClienteAsistenciaAlumno/src/controller/LoginController.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import com.aics.model.SesionUsuario;
+import com.aics.view.PantallaBloqueo;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.time.LocalDateTime;
+
+public class LoginController {
+
+    private PantallaBloqueo vista;
+    private SesionUsuario sesion;
+
+    public LoginController(PantallaBloqueo vista, SesionUsuario sesion) {
+        this.vista = vista;
+        this.sesion = sesion;
+
+        this.vista.setLoginAction(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String user = vista.getUsuario();
+                String pass = vista.getPassword();
+
+                if (sesion.iniciarSesion(user, pass)) {
+                    vista.mostrarMensaje("Bienvenido " + user);
+                    vista.setModoClase(sesion.estaEnHorarioClase());
+                    vista.mostrarPantallaDesbloqueada();
+                } else {
+                    vista.mostrarMensaje("Credenciales inválidas.");
+                }
+            }
+        });
+
+        this.vista.setLogoutAction(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                sesion.cerrarSesion();
+                vista.mostrarMensaje("Sesión cerrada. Tiempo registrado.");
+                vista.mostrarPantallaBloqueo();
+            }
+        });
+    }
+}
